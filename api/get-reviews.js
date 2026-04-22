f70be7b9710b107cde9503ebbf40f8881102a242
lesdareka <lesdareka@gmail.com>
2026-04-22 18:12:24 +0300

    get-reviews.js

--- api/get-reviews.js
+++ api/get-reviews.js
@@ -2,14 +2,22 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+  process.env.SUPABASE_ANON_KEY
 );
 
 export default async function handler(req, res) {
-  const { data } = await supabase
-    .from("reviews")
-    .select("*")
-    .order("created_at", { ascending: false });
+  try {
+    const { data, error } = await supabase
+      .from("reviews")
+      .select("*");
 
-  res.status(200).json(data);
+    if (error) {
+      return res.status(500).json({ error });
+    }
+
+    return res.status(200).json(data);
+
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
 }
